c8e2b9d0f916fdeb3373edd2d6ee89e142369118
BORI <bohhh_@naver.com>
2022-09-23 19:09:57 +0900

    [Login] feat. 체크박스 기능 & 입력 타입 설정

--- app/src/main/java/com/example/bori/SignUp.kt
+++ app/src/main/java/com/example/bori/SignUp.kt
@@ -3,6 +3,7 @@ package com.example.bori
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.ImageButton
 
 class SignUp : AppCompatActivity() {
@@ -15,6 +16,73 @@ class SignUp : AppCompatActivity() {
         arrowToLogin.setOnClickListener {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
+        }
+        val allCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_allCheckBox)
+        val termOfServiceCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_termOfServiceCheckBox)
+        val personalInfoCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_personalInfoCheckBox)
+        val marketingInfoCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_marketingInfoCheckBox)
+
+        allCheckBox.setOnClickListener { agreeCheckBoxHandler("allCheckBox") }
+        termOfServiceCheckBox.setOnClickListener { agreeCheckBoxHandler("termOfServiceCheckBox") }
+        personalInfoCheckBox.setOnClickListener { agreeCheckBoxHandler("personalInfoCheckBox") }
+        marketingInfoCheckBox.setOnClickListener { agreeCheckBoxHandler("marketingInfoCheckBox") }
+
+        val genderCheckBoxMale: androidx.appcompat.widget.AppCompatCheckBox = findViewById(R.id.signUp_genderCheckBoxMale)
+        val genderCheckBoxFemale: androidx.appcompat.widget.AppCompatCheckBox = findViewById(R.id.signUp_genderCheckBoxFemale)
+
+        genderCheckBoxMale.setOnClickListener { genderCheckBoxHandler("Male") }
+        genderCheckBoxFemale.setOnClickListener { genderCheckBoxHandler("Female") }
+
+    }
+
+
+    private fun agreeCheckBoxHandler(checkBoxName: String) {
+        val allCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_allCheckBox)
+        val termOfServiceCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_termOfServiceCheckBox)
+        val personalInfoCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_personalInfoCheckBox)
+        val marketingInfoCheckBox: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_marketingInfoCheckBox)
+        when (checkBoxName) {
+            "allCheckBox" -> {
+                if (allCheckBox.isChecked) {
+                    termOfServiceCheckBox.isChecked = true
+                    personalInfoCheckBox.isChecked = true
+                    marketingInfoCheckBox.isChecked = true
+                } else {
+                    termOfServiceCheckBox.isChecked = false
+                    personalInfoCheckBox.isChecked = false
+                    marketingInfoCheckBox.isChecked = false
+                }
+            }
+            else -> {
+                allCheckBox.isChecked = (
+                        termOfServiceCheckBox.isChecked
+                                && personalInfoCheckBox.isChecked
+                                && marketingInfoCheckBox.isChecked)
+            }
+        }
+    }
+
+    private fun genderCheckBoxHandler(checkBoxName: String) {
+        val genderCheckBoxMale: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_genderCheckBoxMale)
+        val genderCheckBoxFemale: androidx.appcompat.widget.AppCompatCheckBox =
+            findViewById(R.id.signUp_genderCheckBoxFemale)
+
+        when (checkBoxName) {
+            "Male" -> {
+                genderCheckBoxFemale.isChecked = false
+            }
+            "Female" -> {
+                genderCheckBoxMale.isChecked = false
+            }
         }
     }
 }
