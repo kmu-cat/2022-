3e6d64344a91569c6a7cfd1f721dc131e4e86067
BORI <bohhh_@naver.com>
2022-09-29 22:51:08 +0900

    [Main] feat. 하단 네비게이션 구현

---
+++ app/src/main/java/com/example/bori/Home.kt
@@ -0,0 +1,25 @@
+package com.example.bori
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class Home : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+
+
+}
