1227e25030171e8686ead20adc63b81ab41fe459
BORI <bohhh_@naver.com>
2022-10-02 14:26:33 +0900

    [Modals] feat. 모달 창 & x버튼 기능 구현

--- app/src/main/java/com/example/bori/Start.kt
+++ app/src/main/java/com/example/bori/Start.kt
@@ -1,12 +1,18 @@
 package com.example.bori
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 
 class Start : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
@@ -18,6 +24,7 @@ class Start : AppCompatActivity(){
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
 
         val toLogin: TextView = findViewById(R.id.start_toLogin)
 
