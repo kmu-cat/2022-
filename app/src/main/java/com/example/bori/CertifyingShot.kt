de57ff69daa5733a2d689404ac0844f6b8e72820
윤서영 <102743371+ytjdud@users.noreply.github.com>
2022-10-08 13:10:32 +0900

    [back] feat.커뮤니티 기능 구현

--- app/src/main/java/com/example/bori/CertifyingShot.kt
+++ app/src/main/java/com/example/bori/CertifyingShot.kt
@@ -1,21 +1,32 @@
 package com.example.bori
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.bori.databinding.ActivityMainBinding
+import com.example.bori.databinding.FragmentCertifyingShotBinding
+import com.google.firebase.firestore.ktx.toObject
 
 class CertifyingShot : Fragment(){
     lateinit var adaptercommunity: AdapterCommunity
+    lateinit var binding: FragmentCertifyingShotBinding
     val datas = mutableListOf<DataCommunity>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentCertifyingShotBinding.inflate(layoutInflater, container, false)
         val view = inflater.inflate(R.layout.fragment_certifying_shot, container, false)
 
         adaptercommunity = AdapterCommunity(view.context)
@@ -33,5 +44,25 @@ class CertifyingShot : Fragment(){
         return view
     }
 
+    private fun makeRecyclerView(){
+        // 컬렉션을 모두 가져오기
+        MyApplication.db.collection("news")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val item = document.toObject(DataCommunity::class.java)
+                    item.docId=document.id
+                    datas.add(item)
+                }
+                binding.rvProfile.layoutManager= LinearLayoutManager(getActivity())
+                // binding.rvProfile.adapter= MyAdapter(getActivity(), datas)
+
+            }
+            .addOnFailureListener { exception ->
+                Log.d("ImageUpload", "Error getting documents: ", exception)
+                Toast.makeText(getActivity(), "서버로부터 데이터 획득에 실패했습니다.",
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
 
 }
