b55fa42efc77048903de18f47c7599f0d5e8679e
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-11-04 20:20:48 +0900

    [Cat-Setting] feat. '머리' 아이템 RecyclerView를 이용하여 구현
    - 현재 '머리' 아이템 클릭시 종료되는 현상이 있음 (추후 수정 예정)
    - 관련 필요 파일 추가 및 수정

--- app/src/main/java/com/example/bori/CatSettingFragment.kt
+++ app/src/main/java/com/example/bori/CatSettingFragment.kt
@@ -11,9 +11,10 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CatSettingFragment : Fragment() {
-    lateinit var adapteritem: AdapterItem
-    private val datas = mutableListOf<DataItem>()
-    private val datasHair = mutableListOf<DataItemHair>()
+    lateinit var adapteritem_color: AdapterItem
+    lateinit var adapteritem_hair: AdapterItem
+    private val datasColor = mutableListOf<DataItem>()
+    private val datasHair = mutableListOf<DataItem>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,18 +23,24 @@ class CatSettingFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_catsetting, container, false)
 
-        adapteritem = AdapterItem(view.context)
-        val rvColor: RecyclerView = view.findViewById(R.id.rv_item_color)
-        rvColor.adapter = adapteritem
+        adapteritem_color = AdapterItem(view.context)
+        adapteritem_hair = AdapterItem(view.context)
 
-        adapteritem.setItemClickListener(object: AdapterItem.OnItemClickListener{
+        val rvColor: RecyclerView = view.findViewById(R.id.rv_item_color)
+        val rvHair: RecyclerView = view.findViewById(R.id.rv_item_hair)
+
+        rvColor.adapter = adapteritem_color
+        rvHair.adapter = adapteritem_hair
+
+
+        adapteritem_color.setItemClickListener(object: AdapterItem.OnItemClickListener{
             override fun onClick(v: View, position: Int, src: Int) {
                 view.findViewById<ImageView>(R.id.cat_color).setImageResource(src)
                 Toast.makeText(context, "클릭", Toast.LENGTH_SHORT).show()
             }
         })
 
-        datas.apply {
+        datasColor.apply {
             add(DataItem("삼색이", "알록달록 삼색고양이", R.drawable.rc_color_samsagi, R.drawable.cat_samsagi))
             add(DataItem("치즈", "너 입에 우유 묻었어", R.drawable.rc_color_cheese, R.drawable.cat_cheese))
             add(DataItem("호랑이", "어흥 용맹한 호랑이무늬", R.drawable.rc_color_tiger, R.drawable.cat_tiger))
@@ -41,10 +48,15 @@ class CatSettingFragment : Fragment() {
             add(DataItem("샴이", "아궁이 들어갔다옴", R.drawable.rc_color_siamese, R.drawable.cat_siamese))
             add(DataItem("까망이", "밤되면 안보여요", R.drawable.rc_color_black, R.drawable.cat_black))
         }
+        datasHair.apply {
+            add(DataItem("빨간 양갈래 리본", "귀여움이 두배가 돼요", R.drawable.rc_item_ribbon_red, R.drawable.item_test))
+        }
 
-        adapteritem.datas = datas
-        adapteritem.notifyDataSetChanged()
+        adapteritem_color.datas = datasColor
+        adapteritem_hair.datas = datasHair
 
+        adapteritem_color.notifyDataSetChanged()
+        adapteritem_hair.notifyDataSetChanged()
         val myLayoutManager = GridLayoutManager(activity, 5)
         rvColor.layoutManager = myLayoutManager
 
