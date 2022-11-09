5ee8aec7882f7d85a35b9ad86f70818e10a49ea5
BORI <bohhh_@naver.com>
2022-11-09 10:26:32 +0900

    [Refactor] refactor. 개인정보 관리, 비밀번호 변경, 탈퇴 페이지 리팩터링

--- app/src/main/java/com/example/bori/DeleteAccount2.kt
+++ app/src/main/java/com/example/bori/DeleteAccount2.kt
@@ -1,11 +1,20 @@
 package com.example.bori
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class DeleteAccount2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete_account2)
+
+        val backArrow: ImageButton = findViewById(R.id.deleteAccount2_arrow)
+        backArrow.setOnClickListener {
+            val intent = Intent(this, DeleteAccount1::class.java)
+            startActivity(intent)
+        }
     }
 }
