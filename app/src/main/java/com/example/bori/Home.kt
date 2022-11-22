61723b324e2c0a27b673b88d418424f3072b6907
윤서영 <102743371+ytjdud@users.noreply.github.com>
2022-11-22 20:18:42 +0900

    [back] chore.주석해제

--- app/src/main/java/com/example/bori/Home.kt
+++ app/src/main/java/com/example/bori/Home.kt
@@ -18,37 +18,36 @@ class Home : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        val prefs: SharedPreferences? = this.activity?.getSharedPreferences("CatInfo", 0) ?: null
-//        val userCatInfo = prefs?.getString("CatInfo", null)
-//        val userCatInfoJSON = JSONObject(userCatInfo)
-//
-//        var infoColor = userCatInfoJSON.getInt("Color")
-//        var infoHair = userCatInfoJSON.getInt("Hair")
-//        var infoFace = userCatInfoJSON.getInt("Face")
-//        var infoBody = userCatInfoJSON.getInt("Body")
-//        var infoFoot = userCatInfoJSON.getInt("Foot")
-//        var infoEtc = userCatInfoJSON.getInt("Etc")
-//
-//        val view = inflater.inflate(R.layout.fragment_home, container, false)
-//        var dailyComment = view.findViewById<TextView>(R.id.dailyComment)
-//
-//        val userCatColor = view.findViewById<ImageView>(R.id.user_cat_color)
-//        val userCatHair = view.findViewById<ImageView>(R.id.user_cat_hair)
-//        val userCatFace = view.findViewById<ImageView>(R.id.user_cat_face)
-//        val userCatBody = view.findViewById<ImageView>(R.id.user_cat_body)
-//        val userCatFoot = view.findViewById<ImageView>(R.id.user_cat_foot)
-//        val userCatEtc = view.findViewById<ImageView>(R.id.user_cat_etc)
-//
-//        userCatColor.setImageResource(infoColor)
-//        userCatFace.setImageResource(infoFace)
-//        userCatHair.setImageResource(infoHair)
-//        userCatBody.setImageResource(infoBody)
-//        userCatFoot.setImageResource(infoFoot)
-//        userCatEtc.setImageResource(infoEtc)
-//        val commentList = resources.getStringArray(R.array.daily_comment)
-//        val randNum = (0..commentList.size-1).random()
-//        dailyComment.setText(commentList[randNum])
-//
+        val prefs: SharedPreferences? = this.activity?.getSharedPreferences("CatInfo", 0) ?: null
+        val userCatInfo = prefs?.getString("CatInfo", null)
+        val userCatInfoJSON = JSONObject(userCatInfo)
+
+        var infoColor = userCatInfoJSON.getInt("Color")
+        var infoHair = userCatInfoJSON.getInt("Hair")
+        var infoFace = userCatInfoJSON.getInt("Face")
+        var infoBody = userCatInfoJSON.getInt("Body")
+        var infoFoot = userCatInfoJSON.getInt("Foot")
+        var infoEtc = userCatInfoJSON.getInt("Etc")
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        var dailyComment = view.findViewById<TextView>(R.id.dailyComment)
+
+        val userCatColor = view.findViewById<ImageView>(R.id.user_cat_color)
+        val userCatHair = view.findViewById<ImageView>(R.id.user_cat_hair)
+        val userCatFace = view.findViewById<ImageView>(R.id.user_cat_face)
+        val userCatBody = view.findViewById<ImageView>(R.id.user_cat_body)
+        val userCatFoot = view.findViewById<ImageView>(R.id.user_cat_foot)
+        val userCatEtc = view.findViewById<ImageView>(R.id.user_cat_etc)
+
+        userCatColor.setImageResource(infoColor)
+        userCatFace.setImageResource(infoFace)
+        userCatHair.setImageResource(infoHair)
+        userCatBody.setImageResource(infoBody)
+        userCatFoot.setImageResource(infoFoot)
+        userCatEtc.setImageResource(infoEtc)
+        val commentList = resources.getStringArray(R.array.daily_comment)
+        val randNum = (0..commentList.size-1).random()
+        dailyComment.setText(commentList[randNum])
         return view
     }
 }
