f4ac5daf1da1fe473394eb861090e529c0f7b2f3
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-11-23 01:39:24 +0900

    [CatSetting] feat. 아이템 선택 시 사용자에게 이름, 설명 제공
    - 고양이 꾸미기 페이지에서 아이템 선택 시, 해당 아이템의 이름과 간단한 설명이 사용자가 볼 수 있도록 출력
    - onClick 함수 매개인자 변경

--- app/src/main/java/com/example/bori/CatSettingFragment.kt
+++ app/src/main/java/com/example/bori/CatSettingFragment.kt
@@ -3,12 +3,14 @@ package com.example.bori
 
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -84,54 +86,72 @@ class CatSettingFragment : Fragment() {
 
 
         applyButton.setOnClickListener {
-            Toast.makeText(this.context, catInfoJSON.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.context, "꾸미기가 완료되었다옹~", Toast.LENGTH_SHORT).show()
             edit?.putString("CatInfo", catInfoJSON.toString())
             edit?.apply()
         }
 
         adapteritem_color.setItemClickListener(object: AdapterItem.OnItemClickListener{
-            override fun onClick(v: View, position: Int, src: Int) {
+            override fun onClick(v: View, position: Int, src: Int, name: String, explain: String) {
                 view.findViewById<ImageView>(R.id.cat_color).setImageResource(src)
+                view.findViewById<TextView>(R.id.item_name).text = name
+                view.findViewById<TextView>(R.id.item_name).setTypeface(null, Typeface.BOLD)
+                view.findViewById<TextView>(R.id.item_script).text = explain
                 catInfoJSON.remove("Color")
                 catInfoJSON.put("Color", src)
             }
         })
 
         adapteritem_hair.setItemClickListener(object : AdapterItem.OnItemClickListener {
-            override fun onClick(v: View, position: Int, src: Int) {
+            override fun onClick(v: View, position: Int, src: Int, name: String, explain: String) {
                 view.findViewById<ImageView>(R.id.cat_hair).setImageResource(src)
+                view.findViewById<TextView>(R.id.item_name).text = name
+                view.findViewById<TextView>(R.id.item_name).setTypeface(null, Typeface.BOLD)
+                view.findViewById<TextView>(R.id.item_script).text = explain
                 catInfoJSON.remove("Hair")
                 catInfoJSON.put("Hair", src)
             }
         })
 
         adapteritem_face.setItemClickListener(object : AdapterItem.OnItemClickListener {
-            override fun onClick(v: View, position: Int, src: Int) {
+            override fun onClick(v: View, position: Int, src: Int, name: String, explain: String) {
                 view.findViewById<ImageView>(R.id.cat_face).setImageResource(src)
+                view.findViewById<TextView>(R.id.item_name).text = name
+                view.findViewById<TextView>(R.id.item_name).setTypeface(null, Typeface.BOLD)
+                view.findViewById<TextView>(R.id.item_script).text = explain
                 catInfoJSON.remove("Face")
                 catInfoJSON.put("Face", src)
             }
         })
 
         adapteritem_body.setItemClickListener(object : AdapterItem.OnItemClickListener {
-            override fun onClick(v: View, position: Int, src: Int) {
+            override fun onClick(v: View, position: Int, src: Int, name: String, explain: String) {
                 view.findViewById<ImageView>(R.id.cat_body).setImageResource(src)
+                view.findViewById<TextView>(R.id.item_name).text = name
+                view.findViewById<TextView>(R.id.item_name).setTypeface(null, Typeface.BOLD)
+                view.findViewById<TextView>(R.id.item_script).text = explain
                 catInfoJSON.remove("Body")
                 catInfoJSON.put("Body", src)
             }
         })
 
         adapteritem_foot.setItemClickListener(object : AdapterItem.OnItemClickListener {
-            override fun onClick(v: View, position: Int, src: Int) {
+            override fun onClick(v: View, position: Int, src: Int, name: String, explain: String) {
                 view.findViewById<ImageView>(R.id.cat_foot).setImageResource(src)
+                view.findViewById<TextView>(R.id.item_name).text = name
+                view.findViewById<TextView>(R.id.item_name).setTypeface(null, Typeface.BOLD)
+                view.findViewById<TextView>(R.id.item_script).text = explain
                 catInfoJSON.remove("Foot")
                 catInfoJSON.put("Foot", src)
             }
         })
 
         adapteritem_etc.setItemClickListener(object : AdapterItem.OnItemClickListener {
-            override fun onClick(v: View, position: Int, src: Int) {
+            override fun onClick(v: View, position: Int, src: Int, name: String, explain: String) {
                 view.findViewById<ImageView>(R.id.cat_etc).setImageResource(src)
+                view.findViewById<TextView>(R.id.item_name).text = name
+                view.findViewById<TextView>(R.id.item_name).setTypeface(null, Typeface.BOLD)
+                view.findViewById<TextView>(R.id.item_script).text = explain
                 catInfoJSON.remove("Etc")
                 catInfoJSON.put("Etc", src)
             }
@@ -146,14 +166,14 @@ class CatSettingFragment : Fragment() {
             add(DataItem("까망이", "밤되면 안보여요", R.drawable.rc_color_black, R.drawable.cat_black))
         }
         datasHair.apply {
-            add(DataItem("선택 안함", "이잉 싫어요", R.drawable.rc_item_none, R.drawable.item_none))
+            add(DataItem("선택 안함", "이잉 싫어 불편해서 벗었어요", R.drawable.rc_item_none, R.drawable.item_none))
             add(DataItem("빨간 양갈래 리본", "귀여움이 두배가 돼요", R.drawable.rc_item_ribbon_red, R.drawable.item_test))
             add(DataItem("노란 긴꼬리 리본", "바람에 날리는 리본", R.drawable.rc_item_yellow_tail_ribbon, R.drawable.yellow_tail_ribbon))
             add(DataItem("메리 크리스마스", "고양이 맞춤형 모자라구", R.drawable.rc_item_x_mas, R.drawable.x_mas))
             add(DataItem("소라게", "가린 눈에서 눈물 한방울 흐르는 중", R.drawable.rc_item_hermit_crab, R.drawable.hermit_crab))
         }
         datasFace.apply {
-            add(DataItem("선택 안함", "이잉 싫어요", R.drawable.rc_item_none, R.drawable.item_none))
+            add(DataItem("선택 안함", "이잉 싫어 불편해서 벗었어요", R.drawable.rc_item_none, R.drawable.item_none))
             add(DataItem("에취", "에티켓을 지켜요", R.drawable.rc_item_ahchoo, R.drawable.ahchoo))
             add(DataItem("누구인가", "누가 야옹소리를 내었어", R.drawable.rc_item_eye_patch, R.drawable.eye_patch))
             add(DataItem("마이콜 안경", "후루룩 짭짭 후루룩 짭짭", R.drawable.rc_item_michole, R.drawable.michole))
@@ -161,7 +181,7 @@ class CatSettingFragment : Fragment() {
         }
 
         datasBody.apply {
-            add(DataItem("선택 안함", "이잉 싫어요", R.drawable.rc_item_none, R.drawable.item_none))
+            add(DataItem("선택 안함", "이잉 싫어 불편해서 벗었어요", R.drawable.rc_item_none, R.drawable.item_none))
             add(DataItem("공주님 옷", "자세히 보면 레이스 달려있어요", R.drawable.rc_item_body_princess, R.drawable.body_princess))
             add(DataItem("파란 옷", "귀여운 카라가 포인트", R.drawable.rc_item_body_cloth_blue, R.drawable.body_cloth_blue))
             add(DataItem("아야", "아플땐 밴드를 붙여요", R.drawable.rc_item_body_ouch, R.drawable.body_ouch))
@@ -169,14 +189,14 @@ class CatSettingFragment : Fragment() {
         }
 
         datasFoot.apply {
-            add(DataItem("선택 안함", "이잉 싫어요", R.drawable.rc_item_none, R.drawable.item_none))
+            add(DataItem("선택 안함", "이잉 싫어 불편해서 벗었어요", R.drawable.rc_item_none, R.drawable.item_none))
             add(DataItem("맛있는 물고기", "좀 이따가 먹을거야", R.drawable.rc_item_foot_fish, R.drawable.foot_fish))
             add(DataItem("노란 손목 레이스", "따뜻한 손목을 가진 고양이가 되자", R.drawable.rc_item_foot_lace_yellow, R.drawable.foot_lace_yellow))
             add(DataItem("파란 손목 레이스", "따듯한 손목을 가진 고양이가 되자", R.drawable.rc_item_foot_lace_blue, R.drawable.foot_lace_blue))
         }
 
         datasEtc.apply {
-            add(DataItem("선택 안함", "이잉 싫어요", R.drawable.rc_item_none, R.drawable.item_none))
+            add(DataItem("선택 안함", "이잉 싫어 불편해서 벗었어요", R.drawable.rc_item_none, R.drawable.item_none))
             add(DataItem("파란 밥그릇", "배고프다", R.drawable.rc_item_blue_bowl, R.drawable.blue_bowl))
             add(DataItem("주황 밥그릇", "배고프다", R.drawable.rc_item_orange_bowl, R.drawable.orange_bowl))
             add(DataItem("찹쌀떡", "추우니까 양말을 꼭꼭 신고다녀요", R.drawable.rc_item_white_foot, R.drawable.white_foot))
